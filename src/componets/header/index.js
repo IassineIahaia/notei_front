8e8065542c10642cf6914f667390844d7efe161c
Iassine Iahaia <zinhocabila58@gmail.com>
2025-06-06 12:29:12 +0200

    Fazendo o header

---
+++ src/componets/header/index.js
@@ -0,0 +1,12 @@
+import{Navbar, Container} from 'rbx';
+import logoImage from '../../assets/images/logo.jpg';
+
+function Header() {
+    return (
+        <header>
+            <h1>Header</h1>
+        </header>
+    );
+}
+
+export default Header;
